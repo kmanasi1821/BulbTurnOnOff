a79fd6ca3b8c4c74525006d0efe23c90d83a09d2
Manasi <waghmanasi183@gmail.com>
2022-07-19 10:13:42 +0530

    Initial Commit

---
+++ index.js
@@ -0,0 +1,6 @@
+function turnOn() {
+    document.getElementById('bulb-img').src = 'https://www.w3schools.com/js/pic_bulbon.gif'
+}
+function turnOff() {
+    document.getElementById('bulb-img').src = 'https://www.w3schools.com/js/pic_bulboff.gif'
+}
